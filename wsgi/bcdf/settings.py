83b557faff25f74f01fe97d8bd524df751011b9f
DMoney <yoshimitsu12@gmail.com>
2014-07-02 20:53:48 -0400

    Going back to sqlite

--- wsgi/bcdf/settings.py
+++ wsgi/bcdf/settings.py
@@ -28,13 +28,13 @@ if ON_OPENSHIFT:
     # with rhc cartridge add (see /README in this git repo)
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql', # # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'db', # os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'bcdf.db'), # Or path to database file if using sqlite3.
+            'ENGINE': 'django.db.backends.sqlite3', # # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'bcdf.db'), # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'HOST': 'db-bcdf.rhcloud.com',
+            'HOST': '',
             'PORT': '',
-            'USER': 'admind2gJvwt',
-            'PASSWORD': 'YDIj-dDrm7Pr'
+            'USER': '',
+            'PASSWORD': ''
         }
     }
 else:
