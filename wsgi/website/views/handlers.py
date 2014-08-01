e7477f6b2d71d83951180861cfd8389b8efcbcca
DMoney <yoshimitsu12@gmail.com>
2014-07-31 22:22:33 -0400

    Change 404 to raise 404 status code

--- wsgi/website/views/handlers.py
+++ wsgi/website/views/handlers.py
@@ -2,14 +2,20 @@ __author__ = 'adonis'
 
 
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, render, redirect
+from website.models import SpecialArticle
+from website.views.util import article_parse
 
 
 def handle404(request):
     messages.error(request, "The requested page was not found.")
-    return redirect('special:broken')
+    response = get_object_or_404(SpecialArticle, slug='broken-link')
+    (request, context) = article_parse(request, response)
+    return render(request, 'website/special/article.html', context, status=404)
 
 
 def handle500(request):
     messages.error(request, "An error has occurred on the server.")
-    return redirect('home:index')
+    response = get_object_or_404(SpecialArticle, slug='broken-site')
+    (request, context) = article_parse(request, response)
+    return render(request, 'website/special/article.html', context, status=500)
