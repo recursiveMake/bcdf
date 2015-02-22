30dfbf94e698d55deb723e861f97e991e6b70223
DMoney <yoshimitsu12@gmail.com>
2015-02-22 00:47:36 -0500

    Rolling back model change

--- wsgi/website/models.py
+++ wsgi/website/models.py
@@ -79,7 +79,7 @@ class AlertCampaign(Campaign):
 
 
 class CalendarCampaign(Campaign):
-    blurb = models.TextField(blank=True)
+    blurb = models.TextField(null=True)
     recurring = models.BooleanField(default=False)
     link = models.CharField(max_length=64)
 
