197d19dd0e742a20c9d028ac4cd5e97ed0fb2917
Adonis Bovell <Adonis.Bovell@gtri.gatech.edu>
2016-12-11 12:55:34 -0500

    Add video_url migration

---
+++ wsgi/website/migrations/0008_videoarticle_video_url.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('website', '0007_auto_20161211_1026'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='videoarticle',
+            name='video_url',
+            field=models.CharField(default=b'https://www.youtube.com/embed/WmERztRWLEk', max_length=256),
+            preserve_default=True,
+        ),
+    ]
