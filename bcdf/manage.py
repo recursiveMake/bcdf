f0a0292e61d204960e1c8a1a77923785d7673938
Adonis Bovell <adonis.bovell@damballa.com>
2017-09-10 13:11:29 -0400

    Update manage.py to load Django from the correct location

--- bcdf/manage.py
+++ bcdf/manage.py
@@ -2,15 +2,11 @@
 import os
 import sys
 
-ON_OPENSHIFT = False
-if os.environ.has_key('OPENSHIFT_REPO_DIR'):
-    ON_OPENSHIFT = True
+ON_AWS = False
+if os.environ.has_key('AWS'):
+    ON_AWS = True
 
 if __name__ == "__main__":
-    if ON_OPENSHIFT:
-        sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi'))
-    else:
-        sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bcdf.settings")
 
     from django.core.management import execute_from_command_line
