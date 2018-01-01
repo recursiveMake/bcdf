66bdbc4f4788da80713f7f45545f28f456706060
adonisbovell <yoshimitsu12@gmail.com>
2018-01-01 08:42:23 -0500

    Prevent duplicate DEBUG warning messages
    * Explicitly check for single warning message when not on production
    * Remove rhcloud flag

--- website/middleware.py
+++ website/middleware.py
@@ -8,18 +8,26 @@ class ProductionServer(object):
 
     Value will be True if accessed from BCDF URL"""
 
+    WARNING_MESSAGE = "On development server. Unpublished articles are visible"
+
     @classmethod
     def process_request(cls, request):
         host = request.get_host()
         if 'bovellcancerdiabetesfoundation.org' in host or \
                 'bovellfoundation.org' in host:
             request.production = True
-        elif 'web-bcdf.rhcloud.com' in host:
-            request.production = False
         elif 'localhost' in host:
             request.production = False
         else:
             request.production = False
         if not request.production:
-            messages.warning(request, "On development server. Unpublished articles are visible")
+            storage = messages.get_messages(request)
+            warning = False
+            for message in storage:
+                if message.message == ProductionServer.WARNING_MESSAGE:
+                    warning = True
+                    break
+            if not warning:
+                messages.warning(request, ProductionServer.WARNING_MESSAGE)
+            storage.used = False
         return None
